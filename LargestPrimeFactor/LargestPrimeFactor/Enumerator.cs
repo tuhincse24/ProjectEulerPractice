584c0549a9f5168c43a454ffa6104144b68725f1
Tuhin <towhid.tuhin@selise.ch>
2019-08-03 10:10:08 +0600

    Print prime factors.

---
+++ LargestPrimeFactor/LargestPrimeFactor/Enumerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LargestPrimeFactor
+{
+    public class Enumerator : IEnumerable<long>
+    {
+        private static List<long> _primes = new List<long>();
+        private long _lastChecked;
+
+
+        public Enumerator()
+        {
+            _primes.Add(2);
+            _lastChecked = 2;
+        }
+
+
+        private bool IsPrime(long checkValue)
+        {
+            bool isPrime = true;
+
+            foreach (long prime in _primes)
+            {
+                if ((checkValue % prime) == 0 && prime <= Math.Sqrt(checkValue))
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            foreach (long prime in _primes)
+                yield return prime;
+
+            while (_lastChecked < long.MaxValue)
+            {
+                _lastChecked++;
+
+                if (IsPrime(_lastChecked))
+                {
+                    _primes.Add(_lastChecked);
+                    yield return _lastChecked;
+                }
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- LargestPrimeFactor/LargestPrimeFactor/Program.cs
+++ LargestPrimeFactor/LargestPrimeFactor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace LargestPrimeFactor
 {
@@ -6,7 +8,54 @@ namespace LargestPrimeFactor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Enumerator enumerator = new Enumerator();
+
+            bool ok = true;
+
+            while (ok)
+            {
+                Console.WriteLine("Enter number to factorise or Enter to stop.");
+                string entered = Console.ReadLine();
+
+                long toFactorise;
+                ok = long.TryParse(entered, out toFactorise);
+
+                if (ok && toFactorise >= 2)
+                {
+                    StringBuilder factorList = new StringBuilder();
+
+                    foreach (long factor in GetPrimeFactors(toFactorise, enumerator))
+                    {
+                        if (factorList.Length == 0)
+                            factorList.Append("Prime factor(s): ");
+                        else
+                            factorList.Append(", ");
+                        factorList.Append(factor);
+                    }
+
+                    Console.WriteLine(factorList);
+                }
+            }
+        }
+
+
+        private static IEnumerable<long> GetPrimeFactors(long value, Enumerator eratosthenes)
+        {
+            List<long> factors = new List<long>();
+
+            foreach (long prime in eratosthenes)
+            {
+                while (value % prime == 0)
+                {
+                    value /= prime;
+                    factors.Add(prime);
+                }
+
+                if (value == 1)
+                    break;
+            }
+
+            return factors;
         }
     }
 }
